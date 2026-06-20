87a15204206e028192ac52f9d90b1aaaebac0873
yuiisiharashi <yuiisiharashi@gmail.com>
2026-06-20 17:56:44 +0700

    Refactor kirimPesan function and update chat display

--- script.js
+++ script.js
@@ -1,48 +1,32 @@
 const chatBox = document.getElementById("chatBox");
-const API_KEY = AQ.Ab8RN6IelObH3ZZOHO3L1hxrba_0va6J55uXRk2qfGguvQiFDQ
+const API_KEY = "AQ.Ab8RN6IelObH3ZZOHO3L1hxrba_0va6J55uXRk2qfGguvQiFDQ";
+
 const userInput = document.getElementById("userInput");
 const sendBtn = document.getElementById("sendBtn");
 const newChat = document.getElementById("newChat");
 
-function kirimPesan() {
-    const text = userInput.value.trim();
 
-    if (text === "") return;
-
-    chatBox.innerHTML += `
-        <div class="message user">
-            ${text}
-        </div>
-    `;
-
-    userInput.value = "";
-
-    chatBox.innerHTML += `
-        <div class="message ai" id="typing">
-            YUDA.AI sedang mengetik...
-        </div>
-    `;
-
-    chatBox.scrollTop = chatBox.scrollHeight;
-
-    setTimeout(() => {
-        document.getElementById("typing").remove();
 async function kirimPesan() {
+
     const text = userInput.value.trim();
 
     if (text === "") return;
 
+
     chatBox.innerHTML += `
-        <div class="message user">${text}</div>
-    `;
+    <div class="message user">
+        ${text}
+    </div>`;
+
 
     userInput.value = "";
 
+
     chatBox.innerHTML += `
-        <div class="message ai" id="typing">
-            YUDA.AI sedang mengetik...
-        </div>
-    `;
+    <div class="message ai" id="typing">
+        YUDA.AI sedang mengetik...
+    </div>`;
+
 
     const res = await fetch(
     "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=" + API_KEY,
@@ -54,44 +38,43 @@ async function kirimPesan() {
         body:JSON.stringify({
             contents:[{
                 parts:[{
-                    text:`Kamu adalah YUDA.AI. Jawab dengan santai.
-                    
-Pertanyaan:
-${text}`
+                    text:text
                 }]
             }]
         })
     });
 
+
     const data = await res.json();
+
 
     document.getElementById("typing").remove();
 
+
     chatBox.innerHTML += `
-        <div class="message ai">
-            ${data.candidates[0].content.parts[0].text}
-        </div>
-    `;
+    <div class="message ai">
+        ${data.candidates[0].content.parts[0].text}
+    </div>`;
+
 
     chatBox.scrollTop = chatBox.scrollHeight;
 }
-        
-}
+
+
 
 sendBtn.onclick = kirimPesan;
 
+
 userInput.addEventListener("keypress", function(e){
-    if(e.key==="Enter" && !e.shiftKey){
-        e.preventDefault();
+    if(e.key==="Enter"){
         kirimPesan();
     }
 });
 
+
 newChat.onclick = function(){
     chatBox.innerHTML = `
-        <div class="message ai">
-            Halo 👋 Saya YUDA.AI.<br><br>
-            Ada yang bisa saya bantu?
-        </div>
-    `;
+    <div class="message ai">
+        Halo 👋 Saya YUDA.AI
+    </div>`;
 };
