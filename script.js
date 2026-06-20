41b5c9ebcae2712d702471d1b59c6e1a97bd3179
yuiisiharashi <yuiisiharashi@gmail.com>
2026-06-20 18:18:29 +0700

    Refactor kirimPesan function and update API key

--- script.js
+++ script.js
@@ -1,81 +1,68 @@
+const API_KEY = AQ.Ab8RN6JpkmOghiTO3OfxNiYEDGQGf4R1BExsO1utmfgeuCDhzg
 
-const API_KEY = AQ.Ab8RN6ImPHw0ePHg1adfuxa5L7VI8rp__l52UkHno8SPVyNwKg
 const chatBox = document.getElementById("chatBox");
 const userInput = document.getElementById("userInput");
 const sendBtn = document.getElementById("sendBtn");
-const newChat = document.getElementById("newChat");
-
-console.log("YUDA AI JS masuk");
-
-async function kirimPesan() {
-
-    const text = userInput.value.trim();
-
-    if (text === "") return;
 
 
-    chatBox.innerHTML += `
-    <div class="message user">
-        ${text}
-    </div>`;
+async function kirimPesan(){
+
+let text = userInput.value.trim();
+if(!text) return;
 
 
-    userInput.value = "";
+chatBox.innerHTML += `
+<div class="message user">${text}</div>`;
 
 
-    chatBox.innerHTML += `
-    <div class="message ai" id="typing">
-        YUDA.AI sedang mengetik...
-    </div>`;
+userInput.value = "";
 
 
-    const res = await fetch(
-    "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=" + API_KEY,
-    {
-        method:"POST",
-        headers:{
-            "Content-Type":"application/json"
-        },
-        body:JSON.stringify({
-            contents:[{
-                parts:[{
-                    text:text
-                }]
-            }]
-        })
-    });
+try {
 
-
-    const data = await res.json();
-
-
-    document.getElementById("typing").remove();
-
-
-    chatBox.innerHTML += `
-    <div class="message ai">
-        ${data.candidates[0].content.parts[0].text}
-    </div>`;
-
-
-    chatBox.scrollTop = chatBox.scrollHeight;
+let res = await fetch(
+`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`,
+{
+method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
+body:JSON.stringify({
+contents:[
+{
+parts:[
+{
+text:text
 }
-
-
-
-sendBtn.onclick = kirimPesan;
-
-
-userInput.addEventListener("keypress", function(e){
-    if(e.key==="Enter"){
-        kirimPesan();
-    }
+]
+}
+]
+})
 });
 
 
-newChat.onclick = function(){
-    chatBox.innerHTML = `
-    <div class="message ai">
-        Halo 👋 Saya YUDA.AI
-    </div>`;
-};
+let data = await res.json();
+
+console.log(data);
+
+
+chatBox.innerHTML += `
+<div class="message ai">
+${data.candidates[0].content.parts[0].text}
+</div>`;
+
+} catch(e){
+
+chatBox.innerHTML += `
+<div class="message ai">
+Gagal konek Gemini
+</div>`;
+
+console.log(e);
+
+}
+
+}
+
+
+sendBtn.onclick = kirimPesan;
