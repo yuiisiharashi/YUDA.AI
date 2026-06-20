53409407efaac91554f0b633eac31c1c70fb16d1
yuiisiharashi <yuiisiharashi@gmail.com>
2026-06-20 18:24:34 +0700

    Refactor message sending function and update API key

--- script.js
+++ script.js
@@ -1,68 +1,72 @@
-const API_KEY = AQ.Ab8RN6JpkmOghiTO3OfxNiYEDGQGf4R1BExsO1utmfgeuCDhzg
+const API_KEY = AQ.Ab8RN6KqSx9V5YmG_z9WafgZ35-J6FPeU2CdLXAV2ATkbMoGiQ
 
 const chatBox = document.getElementById("chatBox");
 const userInput = document.getElementById("userInput");
 const sendBtn = document.getElementById("sendBtn");
 
 
-async function kirimPesan(){
+sendBtn.onclick = async function(){
 
-let text = userInput.value.trim();
-if(!text) return;
+    let text = userInput.value.trim();
 
-
-chatBox.innerHTML += `
-<div class="message user">${text}</div>`;
+    if(text === "") return;
 
 
-userInput.value = "";
+    chatBox.innerHTML += `
+    <div class="message user">
+    ${text}
+    </div>`;
 
 
-try {
-
-let res = await fetch(
-`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`,
-{
-method:"POST",
-headers:{
-"Content-Type":"application/json"
-},
-body:JSON.stringify({
-contents:[
-{
-parts:[
-{
-text:text
-}
-]
-}
-]
-})
-});
+    userInput.value = "";
 
 
-let data = await res.json();
-
-console.log(data);
-
-
-chatBox.innerHTML += `
-<div class="message ai">
-${data.candidates[0].content.parts[0].text}
-</div>`;
-
-} catch(e){
-
-chatBox.innerHTML += `
-<div class="message ai">
-Gagal konek Gemini
-</div>`;
-
-console.log(e);
-
-}
-
-}
+    chatBox.innerHTML += `
+    <div class="message ai" id="load">
+    YUDA.AI mengetik...
+    </div>`;
 
 
-sendBtn.onclick = kirimPesan;
+    try{
+
+    let response = await fetch(
+    "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key="+API_KEY,
+    {
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify({
+        contents:[{
+          parts:[{
+            text:text
+          }]
+        }]
+      })
+    });
+
+
+    let hasil = await response.json();
+
+    document.getElementById("load").remove();
+
+
+    chatBox.innerHTML += `
+    <div class="message ai">
+    ${hasil.candidates[0].content.parts[0].text}
+    </div>`;
+
+
+    }catch(error){
+
+    document.getElementById("load").remove();
+
+    chatBox.innerHTML += `
+    <div class="message ai">
+    Gemini gagal konek
+    </div>`;
+
+    console.log(error);
+    }
+
+};
